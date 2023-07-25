e928336b88e59a809f7835f51500e6d81be5941a
Daniel Lee <46286577+dlee07@users.noreply.github.com>
2023-07-24 22:19:18 -0400

    Update recipes_app.py

--- recipes_app.py
+++ recipes_app.py
@@ -9,7 +9,7 @@ def display_recipe_table(df):
 
 def main():
     # Replace 'your_recipe_data.xlsx' with the actual path to your Excel file
-    recipe_df = pd.read_excel('/Users/chaniel/Desktop/data_recipes_similarity.xlsx', index_col=0)
+    recipe_df = pd.read_excel('data_recipes_similarity.xlsx', index_col=0)
     pd.set_option('display.max_colwidth', None)
     recipe_df.columns = np.array(['Recipe 1', 'Recipe 2', 'Recipe 3', 'Recipe 4', 'Recipe 5'])
     selected_recipes = st.multiselect("Select up to 3 recipes:", recipe_df.index, default=[])
